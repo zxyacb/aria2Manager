3e333c408f3564bf652b5ff9c3b8416baf0c496c
yecx <yecx@fenbi.com>
2017-11-08 00:17:49 +0800

    init project

---
+++ aria2Manager/Messages.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace aria2Manager
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class BaseMessage
+    {
+        [JsonProperty(PropertyName = "jsonrpc")]
+        public string JsonRPC = "2.0";
+
+        [JsonProperty(PropertyName = "id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id;
+
+        [JsonProperty(PropertyName = "method")]
+        public Aria2Method Method;
+
+        [JsonProperty(PropertyName = "params")]
+        public string[] Params;
+    }
+    
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Aria2Method
+    {
+        [EnumMember(Value = "aria2.addUri")]
+        AddUri,
+        [EnumMember(Value = "aria2.getFiles")]
+        GetFiles,
+        [EnumMember(Value = "aria2.getUris")]
+        GetUris
+    }
+}
---
+++ aria2Manager/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace aria2Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
